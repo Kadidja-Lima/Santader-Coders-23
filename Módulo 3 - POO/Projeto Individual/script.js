a584e7d247971f9a02bdbd04f6c77b2b0671ddba
Kadidja Lima <kadidja.larissa@hotmail.com>
2023-09-13 20:53:06 -0300

    final

--- Módulo 3 - POO/Projeto Individual/script.js
+++ Módulo 3 - POO/Projeto Individual/script.js
@@ -1,22 +1,20 @@
 
+class Catalogo{ 
 
-
-class Catalogo{ // gerencia a interface do usuário e interagi com o HTML
-
-    static #name; //propriedades privadas estáticas da classe que armazena referências ao elemento HTML com IDS.
-    static #price;
+    static #nome; 
+    static #preco;
     static #itens;
 
 
     constructor(){}
 
-        static init(){ //inicializa a classe  e adiciona um ouviente de eventos
-            this.#name = document.querySelector("#name"); //seleciona os elementos Html com Ids
-            this.#price = document.querySelector("#price");
+        static init(){ 
+            this.#nome = document.querySelector("#nome"); 
+            this.#preco = document.querySelector("#preco");
             this.#itens = document.querySelector("#itens");
 
-            this.formCadastro = document.querySelector("form"); //obtem uma referênia ao HTMl e atribui a propriedade formCadastro
-            this.formCadastro.addEventListener("submit", Estoque.cadastrarProdutos); //add o evento de envio do fomr que chama o met da classe Cadastro
+            this.formCadastro = document.querySelector("form"); 
+            this.formCadastro.addEventListener("submit", Estoque.cadastrarProdutos); 
         }
 
         static imprimir(){ 
@@ -25,8 +23,8 @@ class Catalogo{ // gerencia a interface do usuário e interagi com o HTML
                 <div class="card" style="width: 18rem;">
                     <img class="card-img-top" src="./img/cesta1.jpeg" alt="Card image cap">
                     <div class="card-body">
-                        <div id="description"><p>Nome:${value.name}</p></div>
-                        <div id="description"><p>Preço:R$${value.price}</p></div>
+                        <div id="description"><p>Nome:${value.nome}</p></div>
+                        <div id="description"><p>Preço:R$${value.preco}</p></div>
                         <div id="description"><p>Ítens:${value.itens}</p></div>
                         <div id="description"><p>ID:${value.id}</p></div>
                     </div>
@@ -41,6 +39,7 @@ class Catalogo{ // gerencia a interface do usuário e interagi com o HTML
             },"");
             document.querySelector("#itens-product").innerHTML = lista;
             Catalogo.adicionarExcluir();
+            Catalogo.adicionarEventoEditar();
         }
 
         static adicionarExcluir() {
@@ -52,44 +51,76 @@ class Catalogo{ // gerencia a interface do usuário e interagi com o HTML
             });
         }   
 
-        // métodos estáticos de acesso que retornam as propriedades privadas correspondentes.
-
-        static get name() {
-            return this.#name;
+        static adicionarEventoEditar() {
+            document.querySelectorAll(".editar").forEach((botao) => {
+                botao.addEventListener("click", () => {
+                    const idProduto = botao.getAttribute("editar-id");
+                    const produto = Estoque.produtos.find((p) => p.id.toString() === idProduto);
+                    if (produto) {
+                        const novoNome = prompt("Novo nome:", produto.nome);
+                        const novoPreco = prompt("Novo preço:", produto.preco);
+                        const novoItens = prompt("Novos itens:", produto.itens);
+                        if (novoNome !== null && novoPreco !== null && novoItens !== null) {
+                            Estoque.editarProduto(idProduto, novoNome, novoPreco, novoItens);
+                        }
+                    }
+                });
+            });
         }
-        static get price() {
-            return this.#price;
+
+
+        static get nome() {
+            return this.#nome;
+        }
+        static get preco() {
+            return this.#preco;
         }
         static get itens() {
             return this.#itens;
         }
 }
 
-
 let id = 1;
 
 class Produto {
-    #name
-    #price
+    #nome
+    #preco
     #itens
-    constructor(name, price, itens) {
+    constructor(nome, preco, itens) {
         this.id = id++
-        this.#name = name;
-        this.#price = price;
+        this.#nome = nome;
+        this.#preco = preco;
         this.#itens = itens;
     }
 
-    get name() {
-        return this.#name;
+    get nome() {
+        return this.#nome;
     }
-    get price() {
-        return this.#price;
+
+    set nome(novoNome) {
+        this.#nome = novoNome;
+      }
+
+    get preco() {
+        return this.#preco;
     }
+
+    set preco(novoPreco) {
+        this.#preco = novoPreco;
+      }
+
     get itens() {
         return this.#itens;
     }
 
+    set itens(novoItens) {
+        this.#itens = novoItens;
+    }
+
 }
+
+
+
 
 class Estoque{
     static produtos = [];
@@ -98,16 +129,16 @@ class Estoque{
     static cadastrarProdutos(e) {
         e.preventDefault();
         const p = new Produto(
-            Catalogo.name.value,
-            Catalogo.price.value,
+            Catalogo.nome.value,
+            Catalogo.preco.value,
             Catalogo.itens.value
         );
         Estoque.produtos.push(p);
         Catalogo.imprimir();
 
 
-        Catalogo.name.value = "";
-        Catalogo.price.value = "";
+        Catalogo.nome.value = "";
+        Catalogo.preco.value = "";
         Catalogo.itens.value = "";
     }
 
@@ -116,8 +147,16 @@ class Estoque{
         Catalogo.imprimir(); // Atualiza a lista após a exclusão
     }
    
+    static editarProduto(idProduto, novoNome, novoPreco, novoItens) {
+        const produto = Estoque.produtos.find((p) => p.id.toString() === idProduto);
+        if (produto) {
+            produto.nome = novoNome;
+            produto.preco = novoPreco;
+            produto.itens = novoItens;
+            Catalogo.imprimir();
+        }
+    }
 
 }
 
 Catalogo.init();
-
